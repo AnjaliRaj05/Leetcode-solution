ac494346888fc114a74fc18420ee8e6bf1800ba6
AnjaliRaj05 <92683584+AnjaliRaj05@users.noreply.github.com>
2023-03-15 18:28:20 +0530

    Time: 7 ms (43.82%), Space: 10.4 MB (59.30%) - LeetHub

---
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
@@ -0,0 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    bool isCompleteTree(TreeNode* root) {
+        if(root==NULL){
+            return true;
+        }
+
+        int flag=0;
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while(q.size()>0){
+            TreeNode* node=q.front();
+            q.pop();
+            if(node==NULL){
+                flag=1;
+            }
+            else{
+                if(flag==1){
+                    return false;
+                }
+
+                q.push(node->left);
+                q.push(node->right);
+            }
+        }
+
+        return true;
+
+    }
+};
