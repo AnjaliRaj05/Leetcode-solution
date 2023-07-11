24465655f11225c80e5043787c3fdbab47ffbd8d
AnjaliRaj05 <92683584+AnjaliRaj05@users.noreply.github.com>
2023-07-11 20:58:53 +0530

    Time: 35 ms (58.01%), Space: 65.2 MB (45.32%) - LeetHub

--- 2765-longest-alternating-subarray/2765-longest-alternating-subarray.cpp
+++ 2765-longest-alternating-subarray/2765-longest-alternating-subarray.cpp
@@ -4,7 +4,7 @@ public:
     int j = 0, res = 0;
     for (int i = 1; i < nums.size(); ++i) {
         if (nums[i] != nums[j] + (i - j) % 2)
-            j = i - (nums[i - 1]  == nums[i] - 1);
+        j = i - (nums[i - 1]  == nums[i] - 1);
         res = max(res, i - j + 1);
     }
     return res == 1 ? -1 : res;
