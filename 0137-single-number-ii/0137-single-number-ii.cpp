241c7fdd158bc32fb1cf2430d00fc80ef3f2825d
AnjaliRaj05 <92683584+AnjaliRaj05@users.noreply.github.com>
2023-08-09 23:09:33 +0530

    Time: 6 ms (83.54%), Space: 9.5 MB (65.83%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -14,14 +14,45 @@ public:
 //         }
         
 //         return -1;
-        unordered_map<int,int> mp;
-        for(int i=0;i<nums.size();i++)
-        {
-           mp[nums[i]]++; 
+        // unordered_map<int,int> mp;
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //    mp[nums[i]]++; // itrating through mp
+        // }
+        //     for(auto x:mp)
+        //    if(x.second==1)
+        //     return x.first;
+        //        return -1;
+        // int a=0;
+        // int n=nums.size();
+        // for(int i=0;i<n;i++){
+        //     a=a^nums[i];
+        //        }
+        //        return a;
+        // int i=0;
+        // int j=0;
+        // for(int num :nums)
+        // {
+        //     i=(i^num)&~j;
+        //     j=(j^num)&~i;
+        // }
+
+        // return i;
+
+        int ans=0;
+        for(int i=0;i<32;i++){
+            int count=0;
+            for(auto element:nums){
+            if(element&(1<<i))
+                {
+                    count++;
+                }
+            }
+            if(count %3){
+               ans+=(1<<i); 
+            }
         }
-for(auto x:mp)
-    if(x.second==1)
-        return x.first;
-        return -1;
-    }
-};
+        return ans;
+        }
+        };
+  
